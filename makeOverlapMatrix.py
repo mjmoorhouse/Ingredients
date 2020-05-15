2fa35d087101f908ed06d5f71d1dd932bbc3d388
Michael M <micheal@git.com>
2020-05-15 17:06:12 +0100

    Fully usable - and cleaned - prototype version of makeOverlapMatrix.py.  Searches for .CSV input files and combines them into a meta-list excluding  duplicates. Outputs a distance matrix of the overlaps between the lists are outputted and the combined list of all products: "overlapmatrix.txt"; "all_products.txt"

--- makeOverlapMatrix.py
+++ makeOverlapMatrix.py
@@ -16,6 +16,14 @@ import os
 import sys
 import re
 
+overlap_matrix_fname = "overlapmatrix.txt"
+combined_matrix_fname = "all_products.txt"
+
+#These are used to build the mega/meta dataframe: they should sync - ideally via a cofig file at some point -
+#to those in cleanRawDownloads.py
+from pandas import DataFrame
+
+new_column_names = ["Product ID", "Product Name", "URL", "Ingredients", "Allergens", "Weight"]
 
 def main():
     print("Loading Pandas and dependencies:")
@@ -24,7 +32,7 @@ def main():
         import numpy as np
     except:
         sys.exit("Could not load pandas module - check installation and environment is active")
-    #Declare the list storing the IDs explictly.
+    #Declare the list storing the IDs explictly and the 'columns names' based on the none-extension parts of the filenames
     IDs_list = []
     tag_list = list()
     # Set / Test the working directory; stop if it doesn't exist:
@@ -35,6 +43,10 @@ def main():
 
     # Build the name of the distance matrix here so we can log it already:
     dist_matrix_file_path = data_location + "\\distance_mat.txt"
+
+    #Unique-Mega dataframe / table containing all unique product IDs (prefixed with their search origin):
+    mega_data_df = pd.DataFrame(columns=new_column_names)
+
     print("Distance matrix will be output to: '{}'".format(dist_matrix_file_path))
 
     # Use a list comprehension to get all the CSV - hence Data - files, then count them:
@@ -43,18 +55,13 @@ def main():
     if num_data_file_list > 0:
         print("Are [{0}] Files: {1}".format(len(data_file_list), data_file_list))
     else:
-        print("No CSV Files found")
+        print("No CSV Files found - Exiting")
+        sys.exit(0)
 
-    # Do something more interesting - or at least pretend to:
+    # Iterate through the files found:
     for c_file in range(0, num_data_file_list):
         c_file_name = data_file_list[c_file]
-        # File Base Name: the file name without extensions:
 
-        re_match = re.search(re.compile('(?P<tag>^.+)\.'), c_file_name)
-        # re.compile('(?P<weight>[\d.]+)(?P<units>[kK]*[Gg])')
-        c_base_name = re_match.group("tag")
-        print("Tag is: '{}'".format(c_base_name))
-        tag_list.append(c_base_name)
         c_file_path = data_location + "\\" + c_file_name
         print("Processing #{0} of {1}\t: '{2}'".format(c_file + 1, num_data_file_list, c_file_name))
 
@@ -64,46 +71,65 @@ def main():
         # Does it have the column we expect called "Product ID"?
         if not 'Product ID' in data_table.columns:
             print("Cannot find 'ID' column in this ({}) data loaded, skipping".format(c_base_name))
-        these_IDs = data_table['Product ID']
-        #Are the products unique?  Really they need to be for a fair count:
-        if Check_Duplicates(these_IDs):
-            print ("Duplicates in list found ({}) of".format(Check_Duplicates(these_IDs)))
+        #Extract the product IDs into separate list:
+        these_product_ids = data_table['Product ID']
+        # Are the products unique?  Really they need to be for a fair count:
+        if Check_Duplicates(these_product_ids):
+            print("Duplicates in list found ({}) of".format(Check_Duplicates(these_product_ids)))
             continue
-        #All good, so add these IDs to the main list:
-        IDs_list.append(these_IDs)
 
-    # Loading done; some printout to assure us it is working:
+        #Build the tag name (File Base Name + number of items):
+        re_match = re.search(re.compile('(?P<tag>^.+)\.'), c_file_name)
+        c_base_name = re_match.group("tag")
+        #The number of IDs - we use this more than once so:
+        n_in_list = len(these_product_ids)
+        #Build and store the tag name in the format "choc_biscuits(33)" or "crackers(46)"
+        #Note: This is used for displate purposes only in the distance matrix:
+        tag_list.append(c_base_name + "(" + str(n_in_list)+")")
+
+        #Add these IDs to the main list also:
+        IDs_list.append(these_product_ids)
+
+        #Also add these to the other mega/meta analysis matrix:
+        for c_id in range (0, n_in_list):
+            c_id_value = these_product_ids[c_id]
+            if len(mega_data_df.loc[(mega_data_df['Product ID'] == c_id_value)]) == 0:
+                #Remember we need to cast the product IDs as Integers:
+                row = data_table.loc[data_table['Product ID']==int(c_id_value)]
+                mega_data_df = mega_data_df.append(row)
+
+        print ("mega_data_df has {} entries now".format(mega_data_df['Product ID'].count()))
+
+    # Loading done; some printout to assure us it is working by printing stats:
     print ("There are '{}' lists added".format(len(IDs_list)))
-    for x in range (0,len(IDs_list)):
-        print ("List {} has {} elements".format(x, len(IDs_list[x])))
+    [print ("List {} has {} elements".format(x, len(IDs_list[x]))) for x in range (0,len(IDs_list))]
 
-    #Do the overlap
-    dist_matrix = np.zeros((num_data_file_list,num_data_file_list),dtype=np.float)
-    #other_frame = pd.DataFrame({'idx':[range(0,num_data_file_list)], 'cols':[range(0,num_data_file_list)]})
-    other_frame = pd.DataFrame(columns = tag_list, index=tag_list)
-    #other_frame.at[1,2]="Text"
-    print (other_frame)
+    #Do the overlap to compare ID Lists:
+    ###
+    #Create the distance matrix frame:
+    dist_matrix: DataFrame = pd.DataFrame(columns = tag_list, index=tag_list)
 
+    #Score the lists against each other:
+    #(Could be a list comprehension if needed too)
     for x in range(0, len(IDs_list)):
         for y in range(0, len(IDs_list)):
-            print ("{},{}".format(x,y))
-            print ("len {}, {}".format(len(IDs_list[x]),len(IDs_list[y])))
-            other_frame.at[tag_list[x],tag_list[y]] = Score_List(list(IDs_list[x]), list(IDs_list[y]))
-            #dist_matrix[x][y] = Score_List(list(IDs_list[x]), list(IDs_list[y]))
-    print(other_frame)
+            dist_matrix.at[tag_list[x],tag_list[y]] = Score_List(list(IDs_list[x]), list(IDs_list[y]))
 
+    #Write out the distance matrix and the combined table:
+    #To STDOUT:
+    print(dist_matrix)
+
+    #To FS: the distance Matrix:
+    dist_matrix.to_csv(overlap_matrix_fname, sep='\t', index=True)
+    #To FS: The Mega / Meta data frame:
+    mega_data_df.to_csv(combined_matrix_fname, sep ='\t', index=True)
+
+
+#### End of Main Program
 def Score_List(list_a, list_b):
     """
-
-    overlap = setA & setB
-# universe = setA | setB
-
-# total_len = len(listA) + len(listB)
-#
-# d = (2 * len(overlap) / total_len)
-    :param list_a:
-    :param list_b:
-    :return:
+    Basically the count of all the matches / the lengths of the two lists; i.e. classical...except for the 0->2 scale
+    which might get cut back to 0->1 in future.
     """
     setA = set(list_a)
     setB = set(list_b)
@@ -111,46 +137,15 @@ def Score_List(list_a, list_b):
     universe = setA | setB
     d = 2 * len(overlap) / len(universe)
     return d
+
 def Check_Duplicates(data_list):
     """
     Expects a list, returns the number of duplicates found - hence 0 / False if none.
     Creates a 'set' of the list and compares the count - so formally nLog(n) time from memory.
     """
     result = len(data_list) - len(set(data_list))
-    print ("Result of list: {} for {} items".format(result, len(data_list)))
     return result
 
-# Originals:
-# listA = ["Alice", "Bob", "Joe"]
-# listB = ["Joe", "Bob", "Alice", "Ken"]
-
-# Current:
-# listA = ["Alice", "Bob", "Joe", "Kevin", "Julia", "Liam"]
-# listB = ["Joe", "Bob", "Alice", "Ken", "Michelle", "Nick", "Marcel"]
-#
-# setA = set(listA)
-# setB = set(listB)
-#
-# overlap = setA & setB
-# universe = setA | setB
-# total_len = len(listA) + len(listB)
-#
-# d = (2 * len(overlap) / total_len)
-#
-# result1 = float(len(overlap)) / len(setA) * 100
-# result2 = float(len(overlap)) / len(setB) * 100
-# result3 = float(len(overlap)) / len(universe) * 100
-#
-# print ("Result 1: '{}'".format(result1))
-# print ("Result 2: '{}'".format(result2))
-# print ("Result 3: '{}'".format(result3))
-#
-# print ("Total lenght: {}".format(total_len))
-#
-# print ("Distance: {:.2f}".format(d))
-
+#This construct to allow functions in any order:
 if __name__ == '__main__':
-    # contains_result_str = "Fluffy"
-    # al_string = re.sub(r"Contains",  contains_result_str, "Contains Nothing")
-    # print ("Result: '{}'".format(al_string))
     main()
