dded5700ebddb2dcb0419c107c22a18205cb20e0
Michael M <micheal@git.com>
2020-05-20 15:58:26 +0100

    Addition of the HTML Table creation - including CSS styles that may need generalising.

--- assessDataCompleteness.py
+++ assessDataCompleteness.py
@@ -42,14 +42,65 @@ for c_group, this_group in products_df.groupby(['Source']):
     print ("Complete(of which corrected): total : \t{}\t({}) : \t{}".
            format(complete_ingredients, n_corrected_ingredients, n_ingredients))
     #Note down this information into a list (cf. DataFrame - which we build from the list ultimately for performance)
-    group_counts.append({"Name" : str(c_group),
+    group_counts.append({"Product Class" : str(c_group),
                          "Total Products" : int(n_ingredients),
                          "Ingredients" : int(complete_ingredients),
                          "Corr. Ingredients" : int(n_corrected_ingredients)})
 
 #Create the DataFrame for ease of printing and manipulation:
 group_counts_df = pd.DataFrame (group_counts,
-                                columns=["Name", "Total Products", "Ingredients", "Corr. Ingredients"]  )
+                                columns=["Product Class", "Total Products", "Ingredients", "Corr. Ingredients"]  )
 #Printout locally prior to pretty graphing:
 print ("Ultimately:")
 print (group_counts_df)
+
+
+"""
+Pretty Print 2/3: The HTML formatted table
+
+"""
+
+figure_caption = "Completeness of Data Download of Ingredients"
+
+#As we work a lot with the style object - create a short cut:
+df_style_obj = group_counts_df.style
+df_style_obj.set_caption(figure_caption)
+styles = [
+    dict(selector="table", props =[("border-spacing", "0"),
+                                   ("padding","0"),
+                                   ("border-collapse","collapse")]),
+    dict(selector="td", props =[("text-align", "center"),
+                                ("padding","0px"),
+                                  ("font-family", "\"Lucida Console\", Courier, monospace")]),
+    dict(selector=".col0",props=[("background-color", "DarkOrange"),
+                                 ("color", "White"),
+                                 ("font-size", "120%"),
+                                 ("text-align", "left"),
+                                 ("border-right", "2px solid #000"),
+                                 ("font-family", "\"Times New Roman\", Times, serif")]),
+    dict(selector=".col1,.col2,.col3", props = [("text-align","center")]),
+    dict(selector="tr:nth-child(even)",props = [("background-color","#f2f2f2")]),
+    dict(selector=".row_heading", props =[("border-right", "2px solid #000"),
+                                ("font - size", "120 %"),
+                                ("text-align","left"),
+                                ("width", "5em"),
+                                ("background-color","DarkOrange"),
+                                ("color","White")]),
+    dict(selector="caption", props=[("caption-side", "bottom"),
+                                    ("color", "#bbb")]),
+    dict(selector=".col_heading", props=[("font-size", "120%"),
+                                              ("text-align", "center"),
+                                              ("background-color", "DarkOrange"),
+                                              ("color", "White"),
+                                              ("border-bottom", "2px solid #000"),
+                                              ("width", "10em"),
+                                              ("text-align", "center")])
+   ]
+df_style_obj.set_table_styles(styles)
+#df_style_obj.format("{:,.0f}")
+df_style_obj.hide_index()
+print (df_style_obj.render())
+file2 = open(r"table_example.html","w+")
+file2.write(df_style_obj.render())
+file2.close()
+#print (df.style)
