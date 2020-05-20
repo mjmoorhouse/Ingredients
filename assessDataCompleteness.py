0311dfd7d9d48973dd3ef2937075848cf2b9d83a
Michael M <micheal@git.com>
2020-05-20 14:34:18 +0100

    Simplification of code using groupby() in iterative loop specifically.  sys module now imported.

--- assessDataCompleteness.py
+++ assessDataCompleteness.py
@@ -10,6 +10,7 @@ n_corrected_ingredients = int(this_group["Comments"].str.count("Manual Ingredien
 
 """
 import pandas as pd
+import sys
 
 #User servicable parts:
 manual_ingredients_tag = "Manual Ingredients"
@@ -25,19 +26,12 @@ except:
 
 #Print the first few lines, convince us it works:
 print (products_df.head())
-#Group the data by 'source' (i.e. food type)
-grouped_by_source = products_df.groupby(['Source'])
 
-#Split into groups for easy iteration (key: object reference to group)
-groups = dict (list(grouped_by_source))
 #Main results store; we create a DF from this ultimately
 group_counts = []
 
 #For all the groups:
-for c_group in groups:
-    #Get the group we are interested in along with columns: Ingredients & Comments
-    this_group = grouped_by_source[["Ingredients", "Comments"]].get_group(c_group)
-
+for c_group, this_group in products_df.groupby(['Source']):
     #Count the ingredients and determine which we have data for:
     n_ingredients = len(this_group["Ingredients"])
     missing_ingredients = this_group["Ingredients"].isna().sum()
@@ -45,17 +39,17 @@ for c_group in groups:
 
     #Count the comments noting manual entry of ingredients "Manual Ingredients"
     n_corrected_ingredients = int(this_group["Comments"].str.count("Manual Ingredients").sum())
-    print ("Complete(of which corrected): total : \t{}\t({}) : \t{}".format(complete_ingredients, n_corrected_ingredients,
-                                                                        n_ingredients))
+    print ("Complete(of which corrected): total : \t{}\t({}) : \t{}".
+           format(complete_ingredients, n_corrected_ingredients, n_ingredients))
     #Note down this information into a list (cf. DataFrame - which we build from the list ultimately for performance)
     group_counts.append({"Name" : str(c_group),
                          "Total Products" : int(n_ingredients),
                          "Ingredients" : int(complete_ingredients),
                          "Corr. Ingredients" : int(n_corrected_ingredients)})
 
+#Create the DataFrame for ease of printing and manipulation:
 group_counts_df = pd.DataFrame (group_counts,
                                 columns=["Name", "Total Products", "Ingredients", "Corr. Ingredients"]  )
-
 #Printout locally prior to pretty graphing:
 print ("Ultimately:")
 print (group_counts_df)
