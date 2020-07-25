97da016ee6f07cee5e525db425c4f88deaf382bc
Michael M <micheal@git.com>
2020-07-25 17:10:26 +0100

    Initial commit of highlightIngredientFrequency.py demonstrating the loading of the frequency counts DF.

---
+++ highlightIngredientFrequency.py
@@ -0,0 +1,102 @@
+"""
+highlightIngredientFrequency.py
+
+
+In the log plot of ingredient frequency, highlights the location of the ingredients in the 'decay curve' specificied
+in the config.ini.
+
+Outputs a PNG graphic of high quality to either the 'tag' file name or that specified in config.ini
+
+"""
+
+import ingredients as ind
+import configparser
+import pandas as pd
+import sys
+import seaborn as sb
+import matplotlib.pyplot as plt
+import argparse as argparser
+import datetime
+# Get the rank order file:
+rankOrderFile = "ingredients_rank_order.txt"
+"""
+i.e. the one that looks like this:
+	Ingredient	Count
+0	Salt	650
+1	Sugar	512
+2	Water	425
+3	Thiamin	353
+4	Wheat Flour	344
+5	Iron	324
+6	Niacin	317
+7	Milk	308
+"""
+
+def main():
+    """
+    Get the config file: this code needs to be factorised away into the module (assummption scoping won't cause too
+    much passing of parameters about....but for now, verbatim from "mineIngredients.py"
+    """
+    # The ingredients we are going to search for (populate later) Get a product list locally for now from a function (ultimately likely a tab delimited or JSON file):
+    # As all these might be set in the config file, declare them in first in the wider scope:
+    query_ingredients_list = list()
+    config_file = ""
+    search_tag = ""
+    # Do we have command line arguments at all?
+    if len(sys.argv) > 1:
+        # Ok - go get the the config file...
+
+        parser = argparser.ArgumentParser()
+        parser.add_argument('--config', action="store")
+        details = parser.parse_args()
+        config_file = details.config
+        # ....there is an implicit assumption that if the --config switch is being used then it should be expected
+        # that a config file is at the end of it.
+        import configparser
+        print("Using config file ('{}')".format(config_file))
+
+        try:
+            config = configparser.ConfigParser()
+            config.read(config_file)
+            print("Config file: read successful")
+            # Try to get the items we need - all are optional:
+            # The Ingredients search list:
+            if config['DEFAULT'].get('Ingredients'):
+                query_ingredients_list = config['DEFAULT']['Ingredients'].split(",")
+            # The outputfile tag:
+            if config['DEFAULT'].get('OutputFileTag'):
+                search_tag = config['DEFAULT']['OutputFileTag']
+            else:
+                # ....We weren't given it so ... Build the search tag from the list of ingredients we are searching for:
+                search_tag = "".join([x[0:3] for x in query_ingredients_list])
+                # Change any quotes to a "%" to prevent problems with the OS:
+                search_tag = search_tag.replace("'", "-")
+        except Exception as s:
+            print("Tried to read config file ('{}') and failed; exiting".format(config_file))
+            print("(Formally exception error was: '{}'".format(str(s)))
+            sys.exit(1)
+        #
+    else:
+        # Nothing passed - so run with the internal hard coded list:
+        print("Using internal ingredients list: '{}'".format(ind.example_product_list()))
+        query_ingredients_list = ind.example_product_list()
+    print("Using ingredients list and search tag: '{}' & '{}'".format(query_ingredients_list, search_tag))
+    try:
+        counts_df = pd.read_csv(rankOrderFile,sep='\t')
+    except Exception as S:
+        print ("ERROR: Cannot read the counts of ingredient ranks '{}'".format(rankOrderFile))
+        print (S)
+        sys.exit(1)
+    # If we are loading from FS then reseting column names and the index might be useful
+    if 'Unnamed: 0' in counts_df.columns:
+        print ("Resetting column name to 'Rank' and reindexing ")
+        counts_df.rename(columns={'Unnamed: 0':'Rank'}, inplace=True)
+        counts_df.set_index('Rank',inplace=True)
+
+    print (len(counts_df))
+    print (counts_df.columns)
+
+    print (counts_df.head())
+#This construct to allow functions in any order:
+if __name__ == '__main__':
+    main()
